22a080666086608c3b2d838f758273940c00f148
Youcef <youcefboutiche@hotmail.fr>
2026-09-23 23:14:21 +0200

    init english project

---
+++ app_english/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppEnglishConfig(AppConfig):
+    name = 'app_english'
---
+++ app_english/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpRequest, HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hello world from english station")
+
